97438fe80faf5528b4ca7009608c472ec336a229
ivy <6510615211@student.tu.ac.th>
2025-03-19 14:33:02 +0700

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 19/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
